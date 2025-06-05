d76820ed5ca31a47e191d0cd283d4a8b60f5ab76
rakesh-falcon <rakesh@falconsystem.com>
2025-06-05 17:25:09 +0530

    added driver id as mongo objectId

--- app/models/loss_run.py
+++ app/models/loss_run.py
@@ -1,11 +1,16 @@
+# app/models/loss_run.py
 from beanie import Document
 from pydantic import Field
 from datetime import date
+from bson import ObjectId
 
 class LossRun(Document):
-    driver_id: str = Field(...)
+    driver_id: ObjectId = Field(...)
     loss_date: date = Field(...)
     amount: float = Field(...)
 
     class Settings:
         name = "lossruns"
+
+    class Config:
+        arbitrary_types_allowed = True
